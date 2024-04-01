4060dee9b1711b58f8f441f98bb5065806b31445
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-04-01 13:56:10 +0200

    init

---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "selector",
+  content: ["./src/tailwind", "./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        card: {
+          500: "rgb(58 66 84 / 0.95)",
+        },
+        fore: {
+          500: "#28292b",
+        },
+        or: {
+          500: "#eed974",
+        },
+      },
+
+      fontSize: {
+        "2xl": "1.5rem",
+        title: "12vw",
+        "clamp-title": "clamp(10vw, 12vw, 13vw)",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    ({ addVariant }) => {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+    ({ addVariant, e, postcss }) => {
+      addVariant("hover", ({ container, separator }) => {
+        const hoverRule = postcss.atRule({ name: "media", params: "(hover: hover)" });
+        hoverRule.append(container.nodes);
+        container.append(hoverRule);
+        hoverRule.walkRules((rule) => {
+          rule.selector = `.${e(`hover${separator}${rule.selector.slice(1)}`)}:hover`;
+        });
+      });
+    },
+    ({ addBase, theme }) => {
+      function extractColorVars(colorObj, colorGroup = "") {
+        return Object.keys(colorObj).reduce((vars, colorKey) => {
+          const value = colorObj[colorKey];
+
+          const newVars =
+            typeof value === "string"
+              ? { [`--color${colorGroup}-${colorKey}`]: value }
+              : extractColorVars(value, `-${colorKey}`);
+
+          return { ...vars, ...newVars };
+        }, {});
+      }
+
+      addBase({
+        ":root": extractColorVars(theme("colors")),
+      });
+    },
+  ],
+};
